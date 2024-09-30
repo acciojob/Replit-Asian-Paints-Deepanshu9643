22b6d2de8059a81cb1f78d194b8de002cebd7ab2
Deepanshu9643 <deepanshukhorwal7@gmail.com>
2024-09-30 15:31:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+document.querySelector("#change_button").addEventListener("click",function() {
+	  const num = document.querySelector("#block_id").value
+	const color = document.querySelector("#color_id").value
+
+	const griditem =  document.querySelectorAll(".grid-item")
+	griditem.forEach(item => {
+		item.style.backgroundColor = "transparent"
+	})
+
+const block = document.getElementById(num)
+	if(block){
+		block.style.backgroundColor = color
+	}else{
+		alert("Please choose a Number")
+	}
+		})
+
+	document.querySelector("#reset").addEventListener("click",function() {
+		const griditem =  document.querySelectorAll("grid-item")
+		 griditem.forEach(reset => {
+			 reset.style.backgroundColor = "transparent"
+		 })
+		})
